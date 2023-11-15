afc56313554898746eee1bfe23a45aedaf116e68
UmbertoSerino <serumberto@gmail.com>
2023-11-15 16:46:41 +0100

    created const-input

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,21 @@
 // partiamo con l'esercizio delle mail 
 
 
+// ci creiamo un arrey con una lista di mail fittizie
+const listEmail = ["email@sbagliata.it", "secondemail@sbagliata.it", "sbagliancora@errata.it", "email.giusta@mail.it"]; 
+
+
+const userEmail = document.querySelector("input.email");
+const buttonSend = document.querySelector("input-button");
+
+
 // ci creiamo sul html un form dove il cliente inserirà i dati e al click ci prendiamo quei dati e li confrontiamo con quelli della lista dell'array
 
 // dobbiamo creare una condizione dove, se la mail coincide con la lista, al cliente arriva un messaggio di login, altrimenti se non coincide arriva un messaggi di errore o ritenta
 
 
-// ci creiamo un arrey con una lista di mail fittizie
+
+
+
+
+
