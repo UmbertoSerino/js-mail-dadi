6cb4909039cf0f2896450f280a8f942ef5d417e3
UmbertoSerino <serumberto@gmail.com>
2023-11-15 16:29:26 +0100

    created folder and file html and js

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+// partiamo con l'esercizio delle mail 
+
+
+// ci creiamo sul html un form dove il cliente inserirà i dati e al click ci prendiamo quei dati e li confrontiamo con quelli della lista dell'array
+
+// dobbiamo creare una condizione dove, se la mail coincide con la lista, al cliente arriva un messaggio di login, altrimenti se non coincide arriva un messaggi di errore o ritenta
+
+
+// ci creiamo un arrey con una lista di mail fittizie
